821312917fad43c6c9b8182f7a567ac56098bb95
RagingGrim <armandconnor.duplooy@gmail.com>
2016-08-15 19:33:42 +0200

    Started working on abstract factory

---
+++ AbstractFactory/AbstractFactory.cpp
@@ -0,0 +1,25 @@
+/*
+Connor Armand du Plooy
+
+Abstract factory:
+
+Participants:
+      Abstract factory: (Creator) Creates a concrete factory.
+      Concrete factory: Can produce AProductA or AProductB ; which is to say different classes
+
+      Abstract Products: Should be something like a parent class to the concrete products.
+      Concrete Products: Final class of the parent class.
+
+      Client: Uses interfaces defined by the AbstractFactory and AbstractProduct
+
+Imagine adding another layer to the factory method.
+
+This example is on dresses :\
+*/
+
+#include <iostream>
+
+int main(){
+
+      return 0;
+}
