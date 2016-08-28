231ce11db4799aba95cc8849e7833c6f7fcf098e
Raymond <devosray@gmail.com>
2016-08-28 12:36:28 +0200

    Testing tab vs spacing

--- Strategy/Armand/Strategy.cpp
+++ Strategy/Armand/Strategy.cpp
@@ -9,119 +9,118 @@
 
 using namespace std;
 /*
-Connor Armand du Plooy
+	Connor Armand du Plooy
 
-Strategy method:
-Used for encapsulating algorithms ( sort of like every OOP program ever ).
+	Strategy method:
+	Used for encapsulating algorithms ( sort of like every OOP program ever ).
 
-Participants:
-      Context : Uses strategy to use the algorithm.
-      Strategy : Interface to the concrete strategies.
+	Participants:
+	Context : Uses strategy to use the algorithm.
+	Strategy : Interface to the concrete strategies.
 
-      Concrete Strategies : Implements the strategy.
+	Concrete Strategies : Implements the strategy.
 
-Strategy implements the entire algorithm.
+	Strategy implements the entire algorithm.
 
-It would seem that Context encapsulates everything
+	It would seem that Context encapsulates everything
 
-#CMasterRace #CMasterRace #CMasterRace #CMasterRace
+	#CMasterRace #CMasterRace #CMasterRace #CMasterRace
 
-The example I'm using parses a file using either the pure C method or
-the C++ implementation.
+	The example I'm using parses a file using either the pure C method or
+	the C++ implementation.
 */
 
 class ReaderStrategy{
-      public:
-            ReaderStrategy(){};
-            virtual ~ReaderStrategy(){};
-            virtual void execute() = 0;
-
+	public:
+		ReaderStrategy(){};
+		virtual ~ReaderStrategy(){};
+		virtual void execute() = 0;
 };
 
 
 class ReaderContext{
-      public:
-            ReaderContext(){
-                  _caller=NULL;
-            };
-            ~ReaderContext(){};
-            void useMethod(short method , std::string filename);
-            void execute(){
-                  if(_caller){
-                        _caller->execute();
-                  }
-            }
-      private:
-            std::string filename;
-            short method;
-            ReaderStrategy *_caller;
+	public:
+		ReaderContext(){
+			_caller=NULL;
+		};
+		~ReaderContext(){};
+		void useMethod(short method , std::string filename);
+		void execute(){
+			if(_caller){
+				_caller->execute();
+			}
+		}
+	private:
+		std::string filename;
+		short method;
+		ReaderStrategy *_caller;
 };
 
 
 class RawReader : public ReaderStrategy{
-      public:
-            RawReader(std::string filename){
-                  this->filename = filename;
-            }
-            ~RawReader(){
+	public:
+		RawReader(std::string filename){
+			this->filename = filename;
+		}
+		~RawReader(){
 
-            }
-      private:
-            virtual void execute(){
-                  std::cout << "RAW READ: " << std::endl;
+		}
+	private:
+		virtual void execute(){
+			std::cout << "RAW READ: " << std::endl;
 
-                  fd = open(filename.c_str(),O_RDONLY);
+			fd = open(filename.c_str(),O_RDONLY);
 
-                  char *a = new char[250];
+			char *a = new char[250];
 
-                  a[read(fd, a, 250)] = 0; //very hacky code , doesn't parse until the end of a file.
+			a[read(fd, a, 250)] = 0; //very hacky code , doesn't parse until the end of a file.
 
-                  cout << a;
+			cout << a;
 
-                  close(fd);
-            }
-            int fd;
-            std::string filename;
+			close(fd);
+		}
+		int fd;
+		std::string filename;
 };
 
 class BufferedReader : public ReaderStrategy{
-      public:
-            BufferedReader(std::string filename){
-                  std::cout << "BUFFERED READ: " << std::endl;
-                  fs.open(filename.c_str());
-                  while(!fs.eof()){
-                        std::string Buffer;
-                        getline(fs,Buffer);
-                        std::cout << Buffer;
-                  }
-                  fs.close();
-            }
-      private:
-            virtual void execute(){
+	public:
+		BufferedReader(std::string filename){
+			std::cout << "BUFFERED READ: " << std::endl;
+			fs.open(filename.c_str());
+			while(!fs.eof()){
+				std::string Buffer;
+				getline(fs,Buffer);
+				std::cout << Buffer;
+			}
+			fs.close();
+		}
+	private:
+		virtual void execute(){
 
-            }
-            fstream fs;
-            std::string filename;
+		}
+		fstream fs;
+		std::string filename;
 };
 
 void ReaderContext::useMethod(short method , std::string filename){
-      this->filename = filename;
-      this->method = method;
+	this->filename = filename;
+	this->method = method;
 
-      if(_caller)
-            delete _caller;
-      if(method == 1)
-            _caller = new RawReader(filename);
-      if(method == 2)
-            _caller = new BufferedReader(filename);
+	if(_caller)
+		delete _caller;
+	if(method == 1)
+		_caller = new RawReader(filename);
+	if(method == 2)
+		_caller = new BufferedReader(filename);
 }
 int main(){
-      ReaderContext fileReader;
-      fileReader.useMethod(1, "lel.c");
-      fileReader.execute();
+	ReaderContext fileReader;
+	fileReader.useMethod(1, "lel.c");
+	fileReader.execute();
 
-      fileReader.useMethod(2,"lel.c");
-      fileReader.execute();
+	fileReader.useMethod(2,"lel.c");
+	fileReader.execute();
 
-      return 0;
+	return 0;
 }
